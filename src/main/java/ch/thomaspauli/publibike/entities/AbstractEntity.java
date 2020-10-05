79881819afbf7e217b7a4222b9b1131be9ba4701
Thomas Pauli <mail@thomaspauli.ch>
2020-10-05 22:58:40 +0200

    Improves the entities.

--- src/main/java/ch/thomaspauli/publibike/entities/AbstractEntity.java
+++ src/main/java/ch/thomaspauli/publibike/entities/AbstractEntity.java
@@ -1,4 +1,16 @@
 package ch.thomaspauli.publibike.entities;
 
 public abstract class AbstractEntity implements IEntity {
+
+    private Long id;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/ch/thomaspauli/publibike/entities/IEntity.java
+++ src/main/java/ch/thomaspauli/publibike/entities/IEntity.java
@@ -1,4 +1,18 @@
 package ch.thomaspauli.publibike.entities;
 
 public interface IEntity {
+
+    /**
+     * Returns the id of the given entity.
+     *
+     * @return The id of the given entity.
+     */
+    Long getId();
+
+    /**
+     * Sets the id for the given entity to set.
+     *
+     * @param id The id for the given entity.
+     */
+    void setId(Long id);
 }

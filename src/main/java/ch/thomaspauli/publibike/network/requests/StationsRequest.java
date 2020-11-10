19ad6f34c39705f1d82f73451f4b4be5efa128f5
Thomas Pauli <mail@thomaspauli.ch>
2020-11-10 09:38:36 +0000

    Removes the old log4j configuration file.

--- src/main/java/ch/thomaspauli/publibike/network/requests/StationsRequest.java
+++ src/main/java/ch/thomaspauli/publibike/network/requests/StationsRequest.java
@@ -2,6 +2,7 @@ package ch.thomaspauli.publibike.network.requests;
 
 import ch.thomaspauli.publibike.entities.Station;
 import ch.thomaspauli.publibike.network.Request;
+
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpRequestBase;
 

d2f2089e6225f80ffc360091fa2acbf12b25aa96
Thomas Pauli <thomas.pauli@it-processing.ch>
2020-11-15 14:42:51 +0100

    Does some small improvements in the url builder util class.

--- src/main/java/ch/thomaspauli/publibike/network/utils/UrlBuilder.java
+++ src/main/java/ch/thomaspauli/publibike/network/utils/UrlBuilder.java
@@ -16,26 +16,28 @@ import org.slf4j.LoggerFactory;
  */
 public class UrlBuilder {
 
+    // logger
     private static final Logger LOG = LoggerFactory.getLogger(UrlBuilder.class);
 
-    private static final Map<RequestType, String> UrlMap = new HashMap<>();
+    private static final Map<RequestType, String> URL_MAP = new HashMap<>();
 
     static {
-        UrlMap.put(RequestType.STATION,     "https://api.publibike.ch/v1/public/stations/%d");
-        UrlMap.put(RequestType.STATIONS,    "https://api.publibike.ch/v1/public/stations");
+        URL_MAP.put(RequestType.STATION,     "https://api.publibike.ch/v1/public/stations/%d");
+        URL_MAP.put(RequestType.STATIONS,    "https://api.publibike.ch/v1/public/stations");
     }
 
     // locked due to utility functionality.
     private UrlBuilder() {}
 
     /**
+     * Build the url with the given parameters and the given {@link RequestType}.
      *
-     * @param type
-     * @param params
-     * @return
+     * @param type The {@link RequestType} for which the {@link URI} should be builded.
+     * @param params The parameters which should be added to the {@link URI}.
+     * @return The builded {@link URI} with the given parameters and the given {@link RequestType}.
      */
     public static URI build(RequestType type, Object... params) {
-        String url = String.format(UrlMap.get(type), params);
+        String url = String.format(URL_MAP.get(type), params);
 
         LOG.trace("Build url string for a " + type + " request: " + url);
 

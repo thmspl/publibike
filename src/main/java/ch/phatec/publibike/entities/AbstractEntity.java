46f21cab150bfef943022387cd92ac2087b363a1
Thomas Pauli <mail@thomaspauli.ch>
2021-07-29 21:06:49 +0200

    Improves the pom plugins for osshr release.

--- src/main/java/ch/phatec/publibike/entities/AbstractEntity.java
+++ src/main/java/ch/phatec/publibike/entities/AbstractEntity.java
@@ -11,33 +11,21 @@ public abstract class AbstractEntity implements IEntity {
 
     private String name;
 
-    /**
-     * @see AbstractEntity@getId()
-     */
     @Override
     public Long getId() {
         return id;
     }
 
-    /**
-     * @see AbstractEntity@setId(Long)
-     */
     @Override
     public void setId(Long id) {
         this.id = id;
     }
 
-    /**
-     * @see AbstractEntity@getName()
-     */
     @Override
     public String getName() {
         return name;
     }
 
-    /**
-     * @see AbstractEntity@setName(String)
-     */
     @Override
     public void setName(String name) {
         this.name = name;
